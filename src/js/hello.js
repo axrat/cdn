15a625df7e88e128a5ecf9d9d46f33e3c9fbdc18
onoie <onoie3@gmail.com>
2018-06-19 11:58:15 +0900

    Update hello.js

--- src/js/hello.js
+++ src/js/hello.js
@@ -1,3 +1,17 @@
 module.exports = function () {
   console.log('HelloWorld');
 };
+function GetScriptParams() {
+    var scripts = document.getElementsByTagName( 'script' );
+    var src = scripts[ scripts.length - 1 ].src;
+    var query = src.substring( src.indexOf( '?' ) + 1 );
+    var parameters = query.split( '&' );
+    var result = new Object("");
+    for( var i = 0; i < parameters.length; i++ ) {
+        var element = parameters[ i ].split( '=' );
+        var paramName = decodeURIComponent( element[ 0 ] );
+        var paramValue = decodeURIComponent( element[ 1 ] );
+        result[ paramName ] = paramValue;
+    }
+    return result;
+}
